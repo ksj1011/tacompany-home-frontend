62a4fbffe6372520539c3d3eac1eeeffb4f010c3
sjkim <ksj911011@gmail.com>
2022-02-02 21:38:31 +0900

    Updates

---
+++ precache-manifest.8e77bae5246dc787e990f2f732c45210.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe3a35b066206cfb1017402350d7108d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "428bed6554cb4270fa0c",
+    "url": "/static/js/2.83aaf5d0.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.83aaf5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee3ae877a04c1eb5ff5d",
+    "url": "/static/js/main.688b0d22.chunk.js"
+  },
+  {
+    "revision": "f9d073117381a4ac9400",
+    "url": "/static/js/runtime-main.b6cf96a5.js"
+  },
+  {
+    "revision": "5cd5755892463c8fff28860ff7d13fa0",
+    "url": "/static/media/bg-hero.5cd57558.png"
+  },
+  {
+    "revision": "bcec988ff6872aa2dd7f425bd3725e57",
+    "url": "/static/media/ceo.bcec988f.jpeg"
+  },
+  {
+    "revision": "7e3cf94350d465f44f8373e1d5b26d6a",
+    "url": "/static/media/chroma.7e3cf943.png"
+  },
+  {
+    "revision": "571bb83bb19decde285b1cef8169a74c",
+    "url": "/static/media/img-contact.571bb83b.jpg"
+  },
+  {
+    "revision": "37fef9b2f1b772b77093eb9b2d840e4f",
+    "url": "/static/media/img-feature.37fef9b2.jpg"
+  },
+  {
+    "revision": "727f3a674995fc4e3abff3159a87d32a",
+    "url": "/static/media/live.727f3a67.png"
+  },
+  {
+    "revision": "b7633649810eb83e0d0215ee62191a33",
+    "url": "/static/media/process.b7633649.png"
+  },
+  {
+    "revision": "a4d13b336d1a4efc853b4795d381177f",
+    "url": "/static/media/with.a4d13b33.png"
+  },
+  {
+    "revision": "ee33ee88bf3fb56503e32787c774af75",
+    "url": "/static/media/work.ee33ee88.jpeg"
+  },
+  {
+    "revision": "b311618a7bb314c44ae8503eccf8e989",
+    "url": "/static/media/youtube.b311618a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tacompany-home-frontend/precache-manifest.322235a74f8db2a13e5f40ffdea759a6.js"
+  "/precache-manifest.8e77bae5246dc787e990f2f732c45210.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tacompany-home-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
